506a5f7691345cce441b4110aa7098a6839aaa54
l.heizinger <l.heizinger@consolinno.de>
2022-06-01 13:29:18 +0200

    First tests

---
+++ build_app.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+mkdir -p ./build
+export ROOT_DIR=$(pwd)
+cd build 
+export BUILD_DIR=$(pwd)
+
+QT_ROOT=/home/lheizinger/Qt/5.15.2/
+export ANDROID_NDK_ROOT=$HOME/Android/Sdk/ndk/21.3.6528147
+export ANDROID_SDK_ROOT=$HOME/Android/Sdk/
+
+$QT_ROOT/android/bin/qmake ${ROOT_DIR}/nymea-app/ -spec android-clang CONFIG+=qtquickcompiler OVERLAY_PATH=${ROOT_DIR}/nymea-app-consolinno-overlay PKG_CONFIG=/usr/bin/pkg-config 'ANDROID_ABIS=armeabi-v7a arm64-v8a'
+make -j$(nproc)
+make -j$(nproc) apk_install_target
+#
+$QT_ROOT/android/bin/androiddeployqt --input $BUILD_DIR/nymea-app/android-consolinno-energy-deployment-settings.json --output $BUILD_DIR/nymea-app/android-build --android-platform android-32 --jdk /usr/lib/jvm/java-8-openjdk-amd64 --gradle
+##$QT_ROOT/android/bin/androiddeployqt --input $(pwd)/nymea-app/android-consolinno-energy-deployment-settings.json --output $(pwd)/nymea-app/android-build --android-platform android-32 --jdk /usr/lib/jvm/java-8-openjdk-amd64 --gradle --aab --jarsigner --sign '******' --storepass '******'
