0d0e1410162633d0383a901188d559c7b1bae3fa
l.heizinger <l.heizinger@consolinno.de>
2022-06-02 10:25:55 +0200

    Test iOS build

--- build_ios.sh
+++ build_ios.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 export ROOT_DIR=$(pwd)
-mkdir -p ./build/android
-cd ./build/android
+mkdir -p ./build/ios
+cd ./build/ios
 export BUILD_DIR=$(pwd)
-qmake ${ROOT_DIR}/nymea-app/ CONFIG+=qtquickcompiler OVERLAY_PATH=${ROOT_DIR}/nymea-app-consolinno-overlay -spec macx-xcode  PKG_CONFIG=/usr/bin/pkg-config
-make -j$(nproc)
+export QT_ROOT=$(which qmake | sed 's|/bin/qmake||g')
+# Patch mkspec to NOT default to legacy build system of xcode
+# See https://stackoverflow.com/questions/69049200/qt-5-12-for-ios-build-system
+sed -i -e 's|<key>BuildSystemType</key>||g' ${QT_ROOT}/mkspecs/macx-xcode/WorkspaceSettings.xcsettings
+sed -i -e 's|<string>Original</string>||g' ${QT_ROOT}/mkspecs/macx-xcode/WorkspaceSettings.xcsettings
+qmake ${ROOT_DIR}/nymea-app/ CONFIG+=qtquickcompiler OVERLAY_PATH=${ROOT_DIR}/nymea-app-consolinno-overlay   PKG_CONFIG=/usr/bin/pkg-config
+make -j $(sysctl -n hw.physicalcpu)
 
