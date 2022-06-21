2c782d8407bdfa869c7651ffeb0ad68d8435e5ec
lheiz <98517717+lheiz@users.noreply.github.com>
2022-06-21 12:52:07 +0200

    Update build_windows.cmd

--- build_windows.cmd
+++ build_windows.cmd
@@ -1,18 +1,9 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-set
-IF "%OVERLAY_REPOSITORY%"=="" (
-  "C:\Qt\5.15.2\msvc2019_64\bin\qmake.exe" -spec win32-msvc BRANDING=%BRANDING% SSL_LIBS="C:\SSL"
-) ELSE (
-  echo "********* Using overlay path"
-  git clone %OVERLAY_REPOSITORY% overlay
-  "C:\Qt\5.15.2\msvc2019_64\bin\qmake.exe" -spec win32-msvc OVERLAY_PATH=%cd%\overlay BRANDING=%BRANDING% SSL_LIBS="C:\SSL"
-)
+"C:\Qt\5.15.2\msvc2019_64\bin\qmake.exe" -spec win32-msvc OVERLAY_PATH=%cd%\overlay BRANDING=%BRANDING% SSL_LIBS="C:\SSL"
 echo "******** running qmake all"
-C:\Qt\Tools\QtCreator\bin\jom.exe qmake_all
+nmake qmake_all
 echo "******** running lrelease"
-C:\Qt\Tools\QtCreator\bin\jom.exe lrelease
+nmake lrelease
 echo "******** running building"
-C:\Qt\Tools\QtCreator\bin\jom.exe -j9
+nmake
 echo "******** running wininstaller"
-C:\Qt\Tools\QtCreator\bin\jom.exe wininstaller
+nmake wininstaller
