9434cf4431db322c2e3d235c66ef7db82ffc62cd
lheiz <98517717+lheiz@users.noreply.github.com>
2022-06-21 13:50:28 +0200

    Create build_windows.ps1

---
+++ build_windows.ps1
@@ -0,0 +1,10 @@
+$ROOT_DIR=$PWD
+mkdir -p build\windows
+cd build\windows
+$BUILD_DIR=$PWD
+
+qmake "$ROOT_DIR\nymea-app" OVERLAY_PATH="$ROOT_DIR\nymea-app-consolinno-overlay" -spec win32-msvc
+nmake qmake_all
+nmake lrelease
+nmake
+nmake wininstaller
