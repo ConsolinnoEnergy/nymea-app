0a4c2da4901ccd8aa0897391bb2e432f86787e8e
l.heizinger <l.heizinger@consolinno.de>
2022-06-22 14:42:50 +0200

    cleaning windows build

--- build_windows.cmd
+++
@@ -1,9 +0,0 @@
-"C:\Qt\5.15.2\msvc2019_64\bin\qmake.exe" -spec win32-msvc OVERLAY_PATH=%cd%\overlay BRANDING=%BRANDING% SSL_LIBS="C:\SSL"
-echo "******** running qmake all"
-nmake qmake_all
-echo "******** running lrelease"
-nmake lrelease
-echo "******** running building"
-nmake
-echo "******** running wininstaller"
-nmake wininstaller
