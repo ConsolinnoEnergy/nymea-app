7b44aad92382ae919862246cabade04bbb1f85d8
lheiz <98517717+lheiz@users.noreply.github.com>
2022-06-22 13:05:29 +0200

    Update build_windows.ps1

--- build_windows.ps1
+++ build_windows.ps1
@@ -7,3 +7,6 @@ qmake "$ROOT_DIR\nymea-app" OVERLAY_PATH="$ROOT_DIR\nymea-app-consolinno-overlay
 nmake qmake_all
 nmake lrelease
 nmake
+aqt.exe install-tool windows desktop tools_ifw
+cp Tools/QtInstallerFramework/4.4/bin/* ../../../Qt/5.15.2/msvc2019_64/bin
+nmake wininstaller
