b52d9c22a24c720b028467409eb4e022763f8385
lheiz <98517717+lheiz@users.noreply.github.com>
2022-07-12 09:28:41 +0200

    Update build_ios.sh

--- build_ios.sh
+++ build_ios.sh
@@ -5,6 +5,8 @@ mkdir -p ./build/ios
 cd ./build/ios
 export BUILD_DIR=$(pwd)
 export QT_ROOT=$(which qmake | sed 's|/bin/qmake||g')
+# Prevent nymea-app from overwriting team info
+sed -i -e 's/QMAKE_MAC_XCODE_SETTINGS += IOS_DEVELOPMENT_TEAM//g' ../../nymea-app/nymea-app/nymea-app.pro
 # Patch mkspec to NOT default to legacy build system of xcode
 # See https://stackoverflow.com/questions/69049200/qt-5-12-for-ios-build-system
 sed -i -e 's|<key>BuildSystemType</key>||g' ${QT_ROOT}/mkspecs/macx-xcode/WorkspaceSettings.xcsettings
@@ -13,11 +15,14 @@ sed -i -e 's|<string>Original</string>||g' ${QT_ROOT}/mkspecs/macx-xcode/Workspa
 #qmake ${ROOT_DIR}/nymea-app/ -spec macx-ios-clang CONFIG+=debug CONFIG+=iphoneos CONFIG+=device  CONFIG+=qml_debug  OVERLAY_PATH=${ROOT_DIR}/nymea-app-consolinno-overlay QMAKE_MAC_XCODE_SETTINGS+=qteam qteam.name="Consolinno Energy GmbH"
 # qteam.value=403387  QMAKE_TARGET_BUNDLE_PREFIX+=hems.consolinno QMAKE_BUNDLE+=energy
 qmake ${ROOT_DIR}/nymea-app/ -spec macx-ios-clang  \
-CONFIG+=iphoneos CONFIG+=device  CONFIG+=qml_debug \
-OVERLAY_PATH=${ROOT_DIR}/nymea-app-consolinno-overlay \
-QMAKE_MAC_XCODE_SETTINGS+=qteam qteam.name="DEVELOPMENT_TEAM" qteam.value=J757FFDWU9  \
+CONFIG+=iphoneos CONFIG+=device  CONFIG+=qml_debug CONFIG+=release \
+QMAKE_MAC_XCODE_SETTINGS=qteam qteam.name="DEVELOPMENT_TEAM" qteam.value=J757FFDWU9  \
 QMAKE_MAC_XCODE_SETTINGS+=qprofile qprofile.name=PROVISIONING_PROFILE_SPECIFIER qprofile.value=c756d899-58fc-497f-ac4a-6bd17bb0f406  \
-QMAKE_XCODE_CODE_SIGN_IDENTITY=iPhone\ Distribution
-QMAKE_TARGET_BUNDLE_PREFIX+=hems.consolinno QMAKE_BUNDLE+=energy 
-make -j $(sysctl -n hw.physicalcpu)
+QMAKE_XCODE_CODE_SIGN_IDENTITY=\""iPhone Distribution\"" \
+OVERLAY_PATH=${ROOT_DIR}/nymea-app-consolinno-overlay \
+QMAKE_TARGET_BUNDLE_PREFIX+=hems.consolinno QMAKE_BUNDLE+=energy
 
+make qmake_all
+make -j $(sysctl -n hw.physicalcpu)
+# First build fails with "BUILD SUCCEEDED" but misses a file. Second make call should really succeed. Not sure what's the problem here.
+make -j $(sysctl -n hw.physicalcpu)
