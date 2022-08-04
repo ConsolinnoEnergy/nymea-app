7ae6f01e8a22dc9a744a83095b6d0055d9079426
lheiz <98517717+lheiz@users.noreply.github.com>
2022-08-04 14:55:48 +0200

    Update build_ios.sh

--- build_ios.sh
+++ build_ios.sh
@@ -11,13 +11,10 @@ sed -i -e 's/QMAKE_MAC_XCODE_SETTINGS += IOS_DEVELOPMENT_TEAM//g' ../../nymea-ap
 # See https://stackoverflow.com/questions/69049200/qt-5-12-for-ios-build-system
 sed -i -e 's|<key>BuildSystemType</key>||g' ${QT_ROOT}/mkspecs/macx-xcode/WorkspaceSettings.xcsettings
 sed -i -e 's|<string>Original</string>||g' ${QT_ROOT}/mkspecs/macx-xcode/WorkspaceSettings.xcsettings
-#qmake ${ROOT_DIR}/nymea-app/ CONFIG+=qtquickcompiler OVERLAY_PATH=${ROOT_DIR}/nymea-app-consolinno-overlay   PKG_CONFIG=/usr/bin/pkg-config
-#qmake ${ROOT_DIR}/nymea-app/ -spec macx-ios-clang CONFIG+=debug CONFIG+=iphoneos CONFIG+=device  CONFIG+=qml_debug  OVERLAY_PATH=${ROOT_DIR}/nymea-app-consolinno-overlay QMAKE_MAC_XCODE_SETTINGS+=qteam qteam.name="Consolinno Energy GmbH"
-# qteam.value=403387  QMAKE_TARGET_BUNDLE_PREFIX+=hems.consolinno QMAKE_BUNDLE+=energy
 qmake ${ROOT_DIR}/nymea-app/ -spec macx-ios-clang  \
 CONFIG+=iphoneos CONFIG+=device  CONFIG+=qml_debug CONFIG+=release \
 QMAKE_MAC_XCODE_SETTINGS=qteam qteam.name="DEVELOPMENT_TEAM" qteam.value=J757FFDWU9  \
-QMAKE_MAC_XCODE_SETTINGS+=qprofile qprofile.name=PROVISIONING_PROFILE_SPECIFIER qprofile.value=c756d899-58fc-497f-ac4a-6bd17bb0f406  \
+QMAKE_MAC_XCODE_SETTINGS+=qprofile qprofile.name=PROVISIONING_PROFILE_SPECIFIER qprofile.value=7791032b-bdec-425c-9cc6-1a994a9468af \
 QMAKE_XCODE_CODE_SIGN_IDENTITY=\""iPhone Distribution\"" \
 OVERLAY_PATH=${ROOT_DIR}/nymea-app-consolinno-overlay \
 QMAKE_TARGET_BUNDLE_PREFIX+=hems.consolinno QMAKE_BUNDLE+=energy
