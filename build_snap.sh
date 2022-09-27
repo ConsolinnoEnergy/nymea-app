447119bb9f64312d872672436546812a469a1fe2
lheiz <98517717+lheiz@users.noreply.github.com>
2022-09-27 14:27:07 +0200

    Feature/add publish (#23)
    * Create build_linux.yml
    
    * Create build_ubuntu.sh
    
    * Update build_linux.yml
    
    * Update build_linux.yml
    
    * Update build_ubuntu.sh
    
    * Update build_ubuntu.sh
    
    * Create publish.yml
    
    * Update publish.yml
    
    * Update build_windows.yml
    
    * Delete publish.yml
    
    * Update build_windows.yml
    
    * Update build_windows.yml
    
    * Update build_windows.yml
    
    * Update build_windows.yml
    
    * Update and rename build_linux.yml to build_snap.yml
    
    * Update build_ubuntu.sh
    
    * Update build_snap.yml
    
    * Update build_snap.yml
    
    * Update build_snap.yml
    
    * Update build_snap.yml
    
    * Update build_ubuntu.sh
    
    * Update build_snap.yml
    
    * Rename build_ubuntu.sh to build_snap.sh
    
    * Made build_snap.sh executable
    
    * Update build_snap.sh
    
    * Update build_snap.yml
    
    * Update build_snap.yml
    
    * Update build_snap.yml
    
    * Update build_ios.yml
    
    * Update build_ios.yml
    
    * Update build_android.yml
    
    * Update build_ios.yml
    
    * Update build_snap.yml
    
    * Update build_windows.yml
    
    * Update build_snap.yml
    
    * Update build_ios.yml
    
    * Update build_android.yml
    
    * Update build_ios.yml
    
    * Update build_ios.yml
    
    * Update build_ios.yml
    
    * Update build_ios.yml

---
+++ build_snap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd nymea-app
+rm snap
+cp -r ../nymea-app-consolinno-overlay overlay
+rm version.txt | ln -s overlay/version.txt version.txt
+ln -s overlay/packaging/ubuntu/snap snap
+sg lxd -c 'snapcraft --use-lxd'
