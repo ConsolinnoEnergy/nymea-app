0e7dace43550991824da1f30f1701d40e902daf3
l.heizinger <l.heizinger@consolinno.de>
2023-01-18 13:21:22 +0100

    Improved building android test version 1/x

--- patch_test_version.sh
+++ patch_test_version.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd nymea-app
 find ./ -iname "*.qml" | xargs -L1 python ../patch_accessibility.py --inplace {}
+cd ..
 cd nymea-app-consolinno-overlay
 find ./ -iname "*.qml" | xargs -L1 python ../patch_accessibility.py --inplace {}
 cd ..
