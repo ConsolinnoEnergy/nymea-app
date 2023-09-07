cd7f1172611fe589753075d05f4e226b3cb50a58
lheiz <98517717+lheiz@users.noreply.github.com>
2023-09-07 09:35:22 +0200

    Update build_appimage.sh

--- build_appimage.sh
+++ build_appimage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-sudo apt install -y libavahi-common-dev  libavahi-client-dev
+sudo apt install -y libavahi-common-dev  libavahi-client-dev rename
 
 export ROOT_DIR=$(pwd)
 mkdir -p ./build/appimage
@@ -14,7 +14,7 @@ fi
 
 MAKE_BIN=make
 LDEPLOYQT=${ROOT_DIR}/linuxdeployqt-*-x86_64.AppImage
-
+COMMIT_HASH=$(git rev-parse --short "$GITHUB_SHA")
 echo "Build tools:"
 echo "qmake: $QMAKE"
 echo "make: $MAKE_BIN"
@@ -31,4 +31,4 @@ make -j$(nproc) INSTALL_ROOT=${BUILD_DIR}/nymea-app/linux-build install
 export VERSION=$(cat ${ROOT_DIR}/nymea-app-consolinno-overlay/version.txt | head -n1 | sed 's/\./-/g')
 cp ${ROOT_DIR}/nymea-app-consolinno-overlay/packaging/appimage/consolinno-energy.desktop ${ROOT_DIR}/build/appimage/nymea-app/linux-build/usr/share/applications/consolinno-energy.desktop
 $LDEPLOYQT ${ROOT_DIR}/build/appimage/nymea-app/linux-build/usr/share/applications/consolinno-energy.desktop  -appimage -qmldir=${ROOT_DIR}/nymea-app -qmldir=${ROOT_DIR}/nymea-app-consolinno-overlay
-
+rename "s/x86/$COMMIT_HASH-x86/g" build/appimage/*.AppImage
