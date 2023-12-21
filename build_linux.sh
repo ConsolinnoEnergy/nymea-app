c9bf639b47d62e315686247f50a1a183ded5e17f
l.heizinger <l.heizinger@consolinno.de>
2023-12-21 10:16:17 +0100

    Added linux develop build script

---
+++ build_linux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+export ROOT_DIR=$(pwd)
+mkdir -p ./build/linux
+cd ./build/linux
+export BUILD_DIR=$(pwd)
+if [[ -z "${QT_ROOT}" ]]; then
+    QMAKE=qmake
+else
+    QMAKE=${QT_ROOT}/gcc_64/bin/qmake
+fi
+
+MAKE_BIN=make
+echo "Build tools:"
+echo "qmake: $QMAKE"
+$QMAKE -v
+echo "make: $MAKE_BIN"
+
+$QMAKE \
+${ROOT_DIR}/nymea-app/nymea-app.pro \
+OVERLAY_PATH=${ROOT_DIR}/nymea-app-consolinno-overlay
+
+make -j$(nproc) qmake_all
+make lrelease
+make -j$(nproc)
+make -j$(nproc) INSTALL_ROOT=${BUILD_DIR}/nymea-app/linux-build install
+
+# If run in args then run the app
+if [[ "$1" == "run" ]]; then
+    echo "Running nymea-app"
+    ${BUILD_DIR}/nymea-app/linux-build/usr/bin/consolinno-energy
+fi
+
