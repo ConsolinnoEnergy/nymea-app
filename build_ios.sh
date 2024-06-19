6943071b117e78c9f496985bb35bae00c31e6e0d
l.heizinger <l.heizinger@consolinno.de>
2024-06-19 15:24:19 +0200

    Update link to Qt macos build

--- build_ios.sh
+++ build_ios.sh
@@ -3,9 +3,9 @@
 set -e
 export ROOT_DIR=$(pwd)
 cd .. 
-wget https://github.com/ConsolinnoEnergy/qt5-builder/releases/download/debug-release/Qt-5.15-14-macos.tar.gz
+wget https://github.com/ConsolinnoEnergy/qt5-builder/releases/download/v5.15.14-build1/Qt-5.15-14-macos-xcode-15.0.1.tar.gz
 mkdir qt-5.15.14
-tar -xzf Qt-5.15-14-macos.tar.gz -C qt-5.15.14
+tar -xzf Qt-5.15-14-macos-xcode-15.0.1.tar.gz -C qt-5.15.14
 cd $ROOT_DIR
 
 
