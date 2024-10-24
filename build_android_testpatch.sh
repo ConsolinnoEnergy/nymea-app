7491d40f00f8fa29fc8cb82a4ab77bcff03e60e3
manueltamm <manuel.tamm@inventive-spirit.com>
2024-10-24 11:55:38 +0200

    Build Consolinno

--- build_android_testpatch.sh
+++ build_android_testpatch.sh
@@ -39,10 +39,17 @@ make lrelease
 make -j$(nproc)
 make -j$(nproc) INSTALL_ROOT=${BUILD_DIR}/nymea-app/android-build install
 
+SETTINGS_JSON=$WHITELABEL_TARGET
+
+if [ "$WHITELABEL_TARGET" == "Consolinno-HEMS" ]; then
+  # for consolinno a different application name is used.
+  SETTINGS_JSON="consolinno-energy"
+fi
+
 if [[ -z "${SELFSIGN}" ]]; then
     # Building unsigned apk and signing it manuallly to use --v2-signing scheme
      $ADEPQT \
-    --input $BUILD_DIR/nymea-app/android-$WHITELABEL_TARGET-deployment-settings.json \
+    --input $BUILD_DIR/nymea-app/android-$SETTINGS_JSON-deployment-settings.json \
     --output $BUILD_DIR/nymea-app/android-build \
     --android-platform android-32 \
     --release \
@@ -56,12 +63,12 @@ if [[ -z "${SELFSIGN}" ]]; then
     --key-pass pass:${SIGNING_KEY_PASSWORD} \
     --v2-signing-enabled  \
     -v \
-    --out $BUILD_DIR/nymea-app/android-build//build/outputs/apk/release/${WHITELABEL_TARGET}-${VERSION}-signed-testing.apk \
+    --out $BUILD_DIR/nymea-app/android-build//build/outputs/apk/release/${SETTINGS_JSON}-${VERSION}-signed-testing.apk \
     $BUILD_DIR/nymea-app/android-build//build/outputs/apk/release/android-build-release-unsigned.apk
 else
     # SELFSIGN env is defined -> build .apk and sign with new keypair
     $ADEPQT \
-    --input $BUILD_DIR/nymea-app/android-$WHITELABEL_TARGET-deployment-settings.json \
+    --input $BUILD_DIR/nymea-app/android-$SETTINGS_JSON-deployment-settings.json \
     --output $BUILD_DIR/nymea-app/android-build \
     --android-platform android-32 \
     --jdk /usr/lib/jvm/java-8-openjdk-amd64 \
@@ -77,7 +84,7 @@ else
     --v2-signing-enabled  \
     --key $BUILD_DIR/key.pk8 --cert $BUILD_DIR/certificate_x509.pem \
     -v \
-    --out $BUILD_DIR/nymea-app/android-build//build/outputs/apk/release/${WHITELABEL_TARGET}-${VERSION}-selfsigned-testing.apk \
+    --out $BUILD_DIR/nymea-app/android-build//build/outputs/apk/release/${SETTINGS_JSON}-${VERSION}-selfsigned-testing.apk \
     $BUILD_DIR/nymea-app/android-build//build/outputs/apk/release/android-build-release-unsigned.apk
 fi
  
--- build_appimage.sh
+++ build_appimage.sh
@@ -24,11 +24,18 @@ $QMAKE \
 ${ROOT_DIR}/nymea-app/nymea-app.pro \
 OVERLAY_PATH=${ROOT_DIR}/nymea-app-consolinno-overlay
 
+SETTINGS_JSON=$WHITELABEL_TARGET
+
+if [ "$WHITELABEL_TARGET" == "Consolinno-HEMS" ]; then
+  # for consolinno a different application name is used.
+  SETTINGS_JSON="consolinno-energy"
+fi
+
 make -j$(nproc) qmake_all
 make lrelease
 make -j$(nproc)
 make -j$(nproc) INSTALL_ROOT=${BUILD_DIR}/nymea-app/linux-build install
 export VERSION=$(cat ${ROOT_DIR}/nymea-app-consolinno-overlay/version.txt | head -n1 | sed 's/\./-/g')
-cp ${ROOT_DIR}/nymea-app-consolinno-overlay/packaging/appimage/$WHITELABEL_TARGET.desktop ${ROOT_DIR}/build/appimage/nymea-app/linux-build/usr/share/applications/$WHITELABEL_TARGET.desktop
-$LDEPLOYQT ${ROOT_DIR}/build/appimage/nymea-app/linux-build/usr/share/applications/$WHITELABEL_TARGET.desktop  -appimage -qmldir=${ROOT_DIR}/nymea-app -qmldir=${ROOT_DIR}/nymea-app-consolinno-overlay
+cp ${ROOT_DIR}/nymea-app-consolinno-overlay/packaging/appimage/$SETTINGS_JSON.desktop ${ROOT_DIR}/build/appimage/nymea-app/linux-build/usr/share/applications/$SETTINGS_JSON.desktop
+$LDEPLOYQT ${ROOT_DIR}/build/appimage/nymea-app/linux-build/usr/share/applications/$SETTINGS_JSON.desktop  -appimage -qmldir=${ROOT_DIR}/nymea-app -qmldir=${ROOT_DIR}/nymea-app-consolinno-overlay
 rename "s/x86/$COMMIT_HASH-x86/g" build/appimage/*.AppImage
