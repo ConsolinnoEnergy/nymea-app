2039aa71bc8ae641ff03ee84b154510b80830e78
l.heizinger <l.heizinger@consolinno.de>
2024-11-07 09:19:48 +0100

    Added whitelabel support for build_linux.sh script

--- build_linux.sh
+++ build_linux.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-# Example usage to start app in german: 
-# QT_ROOT=<PATH_TO_QT>/Qt/5.15.2 ./build_linux.sh run de_DE
+# Example usage to start and build Consolinno-HEMS app in german:
+# QT_ROOT=<PATH_TO_QT>/Qt/5.15.2 ./build_linux.sh Consolinno-HEMS run de_DE
+
 set -e
+
+export WHITELABEL_TARGET=$1
+
+if [ ! -d "./configuration-files/$WHITELABEL_TARGET" ]; then
+    echo "Whitelabel target $WHITELABEL_TARGET does not exist. Available options are:"
+    ls -1 ./configuration-files
+    exit 1
+fi
+
+cd nymea-app-consolinno-overlay
+git clean -f -d -x
+cd ..
+
+./distribute_assets.sh
 
 export ROOT_DIR=$(pwd)
 mkdir -p ./build/linux
@@ -20,8 +35,8 @@ $QMAKE -v
 echo "make: $MAKE_BIN"
 
 $QMAKE \
-${ROOT_DIR}/nymea-app/nymea-app.pro \
-OVERLAY_PATH=${ROOT_DIR}/nymea-app-consolinno-overlay
+    ${ROOT_DIR}/nymea-app/nymea-app.pro \
+    OVERLAY_PATH=${ROOT_DIR}/nymea-app-consolinno-overlay
 
 make -j$(nproc) qmake_all
 make lrelease
@@ -29,8 +44,14 @@ make -j$(nproc)
 make -j$(nproc) INSTALL_ROOT=${BUILD_DIR}/nymea-app/linux-build install
 
 # If run in args then run the app
-if [[ "$1" == "run" ]]; then
+if [[ "$2" == "run" ]]; then
     echo "Running nymea-app"
-    LANGUAGE=$2 ${BUILD_DIR}/nymea-app/linux-build/usr/bin/consolinno-energy
+    # Should be fixed when output binary is called $WHITELABEL_TARGET
+    if [ $WHITELABEL_TARGET == "Consolinno-HEMS" ]; then
+        LANGUAGE=$3 ${BUILD_DIR}/nymea-app/linux-build/usr/bin/consolinno-energy
+    elif [ $WHITELABEL_TARGET == "Q.HOME-CONTROL" ]; then
+        LANGUAGE=$3 ${BUILD_DIR}/nymea-app/linux-build/usr/bin/Q.HOME-CONTROL
+    elif [ $WHITELABEL_TARGET == "Zewo-Dynamics" ]; then
+        LANGUAGE=$3 ${BUILD_DIR}/nymea-app/linux-build/usr/bin/consolinno-energy
+    fi
 fi
-
